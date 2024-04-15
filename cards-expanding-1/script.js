f14139457e5b878e2387c974cdad88b23004e3ae
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-04-16 02:57:52 +0300

    just a line for commentary.

--- cards-expanding-1/script.js
+++ cards-expanding-1/script.js
@@ -23,3 +23,5 @@ function removeActiveClass() {
 }
 
 /* Adding ' removeActiveClass() ' function before adding ' active ' class simply removing all the other ' active ' classes so there won' t be 3- 4 active classes at the same time in the script. */
+
+// ' () is optional for the variable of forEach. It can be just panel. '
